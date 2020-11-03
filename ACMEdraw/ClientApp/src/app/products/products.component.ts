e1c968e77bbc10a19f0232a15f0a177f526ea5c6
ebquhf <barna211@vipmail.hu>
2020-11-03 16:55:41 +0100

    Paginator fixed :D

--- ACMEdraw/ClientApp/src/app/products/products.component.ts
+++ ACMEdraw/ClientApp/src/app/products/products.component.ts
@@ -4,14 +4,14 @@ import { Product } from './product';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 @Component({
-  selector: 'app-Products',
+  selector: 'app-products',
   templateUrl: './Products.component.html',
   styleUrls: ['./Products.component.css']
 })
 export class ProductsComponent {
   public displayedColumns: string[] = ['id', 'name', 'serialNumber'];
   public products: Product[];
-
+  public productSlice: Product[];  
   pageEvent: PageEvent;
   constructor(
     private http: HttpClient,
@@ -21,6 +21,16 @@ export class ProductsComponent {
     this.http.get<Product[]>(this.baseUrl + 'api/Products')
       .subscribe(result => {
         this.products = result;
+        this.productSlice = this.products.slice(0, 10);
       }, error => console.error(error));
+  }
+
+  onPageChange(event: PageEvent) {
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.products.length) {
+      endIndex = this.products.length;
+    }
+    this.productSlice = this.products.slice(startIndex, endIndex); 
   }
 }
